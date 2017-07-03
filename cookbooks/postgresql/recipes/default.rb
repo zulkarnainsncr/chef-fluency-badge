d2acb59e3f45004b9351c00e709c9a7a68340cbd
zul <kagalwalla.zul@gmail.com>
2017-07-03 18:33:50 +0000

    added postgresql cookbook

---
+++ cookbooks/postgresql/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: postgresql
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
+
+package 'postgresql-server' do
+	notifies :run , 'execute[postgresql-init]'
+end
+
+exceute 'postgresql-init' do 
+	command 'postgresql-setup initdb'
+	action:nothing
+end
+
+service 'postgresql' do
+	action [:enable, :start]
+end
